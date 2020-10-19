020ce3e4015d9719df0ea035c40f47ef8421fe9e
annamariejsabandal@gmail.com <annamariejsabandal@gmail.com>
2020-10-19 18:24:18 +0800

    initial commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="hhh LpR fFr">
+
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          The Login Page
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+
+      <q-tabs align="left">
+      </q-tabs>
+    </q-header>
+
+    <q-drawer v-model="right" side="right" overlay elevated>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      right: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scopped></style>
